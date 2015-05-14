0d269b3832a6c747232fa35aefc9e8b7f09b7e1e
Nimisha Asthagiri <nasthagiri@edx.org>
2015-05-14 00:39:03 -0400

    Move generic mobile API view decorators.

--- lms/djangoapps/mobile_api/utils.py
+++ lms/djangoapps/mobile_api/utils.py
@@ -1,61 +1,18 @@
 """
 Common utility methods and decorators for Mobile APIs.
 """
-
-import functools
-from rest_framework import permissions
-from opaque_keys.edx.keys import CourseKey
-from xmodule.modulestore.django import modulestore
-
-from courseware.courses import get_course_with_access
-from openedx.core.lib.api.authentication import (
-    SessionAuthenticationAllowInactiveUser,
-    OAuth2AuthenticationAllowInactiveUser,
-)
-from openedx.core.lib.api.permissions import IsUserInUrl
+from openedx.core.lib.api.view_utils import view_course_access, view_auth_classes
 
 
-def mobile_course_access(depth=0, verify_enrolled=True):
+def mobile_course_access(depth=0):
     """
     Method decorator for a mobile API endpoint that verifies the user has access to the course in a mobile context.
     """
-    def _decorator(func):
-        """Outer method decorator."""
-        @functools.wraps(func)
-        def _wrapper(self, request, *args, **kwargs):
-            """
-            Expects kwargs to contain 'course_id'.
-            Passes the course descriptor to the given decorated function.
-            Raises 404 if access to course is disallowed.
-            """
-            course_id = CourseKey.from_string(kwargs.pop('course_id'))
-            with modulestore().bulk_operations(course_id):
-                course = get_course_with_access(
-                    request.user,
-                    'load_mobile' if verify_enrolled else 'load_mobile_no_enrollment_check',
-                    course_id,
-                    depth=depth
-                )
-                return func(self, request, course=course, *args, **kwargs)
-        return _wrapper
-    return _decorator
+    return view_course_access(depth=depth, access_action='load_mobile')
 
 
 def mobile_view(is_user=False):
     """
     Function and class decorator that abstracts the authentication and permission checks for mobile api views.
     """
-    def _decorator(func_or_class):
-        """
-        Requires either OAuth2 or Session-based authentication.
-        If is_user is True, also requires username in URL matches the request user.
-        """
-        func_or_class.authentication_classes = (
-            OAuth2AuthenticationAllowInactiveUser,
-            SessionAuthenticationAllowInactiveUser
-        )
-        func_or_class.permission_classes = (permissions.IsAuthenticated,)
-        if is_user:
-            func_or_class.permission_classes += (IsUserInUrl,)
-        return func_or_class
-    return _decorator
+    return view_auth_classes(is_user)
--- openedx/core/lib/api/view_utils.py
+++ openedx/core/lib/api/view_utils.py
@@ -1,11 +1,23 @@
 """
 Utilities related to API views
 """
+import functools
 from django.core.exceptions import NON_FIELD_ERRORS, ValidationError
 from django.http import Http404
 
+from rest_framework import permissions
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
+
+from opaque_keys.edx.keys import CourseKey
+from xmodule.modulestore.django import modulestore
+
+from courseware.courses import get_course_with_access
+from openedx.core.lib.api.authentication import (
+    SessionAuthenticationAllowInactiveUser,
+    OAuth2AuthenticationAllowInactiveUser,
+)
+from openedx.core.lib.api.permissions import IsUserInUrl, IsAuthenticatedOrDebug
 
 
 class DeveloperErrorViewMixin(object):
@@ -48,3 +60,49 @@ class DeveloperErrorViewMixin(object):
             return self.make_validation_error_response(exc)
         else:
             raise
+
+
+def view_course_access(depth=0, access_action='load'):
+    """
+    Method decorator for an API endpoint that verifies the user has access to the course.
+    """
+    def _decorator(func):
+        """Outer method decorator."""
+        @functools.wraps(func)
+        def _wrapper(self, request, *args, **kwargs):
+            """
+            Expects kwargs to contain 'course_id'.
+            Passes the course descriptor to the given decorated function.
+            Raises 404 if access to course is disallowed.
+            """
+            course_id = CourseKey.from_string(kwargs.pop('course_id'))
+            with modulestore().bulk_operations(course_id):
+                course = get_course_with_access(
+                    request.user,
+                    access_action,
+                    course_id,
+                    depth=depth
+                )
+                return func(self, request, course=course, *args, **kwargs)
+        return _wrapper
+    return _decorator
+
+
+def view_auth_classes(is_user=False):
+    """
+    Function and class decorator that abstracts the authentication and permission checks for api views.
+    """
+    def _decorator(func_or_class):
+        """
+        Requires either OAuth2 or Session-based authentication.
+        If is_user is True, also requires username in URL matches the request user.
+        """
+        func_or_class.authentication_classes = (
+            OAuth2AuthenticationAllowInactiveUser,
+            SessionAuthenticationAllowInactiveUser
+        )
+        func_or_class.permission_classes = (IsAuthenticatedOrDebug,)
+        if is_user:
+            func_or_class.permission_classes += (IsUserInUrl,)
+        return func_or_class
+    return _decorator
